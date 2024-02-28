d9d6cea73c76908cacce41356a64cd347a7ef838
arskill11 <atrinozenko3@gmail.com>
2024-02-29 01:06:11 +0300

    Made it responsive & added icons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import emailIcon from './email-icon.svg';
+import phoneIcon from './phone-icon.svg';
+// import profilePhoto from './profile-pic.jpeg';
+
+const emailIcomPlaceholder = document.querySelector('.email-icon');
+const phoneIconPlaceholder = document.querySelector('.phone-icon');
+// const profilePhotoPlaceholder = document.querySelector('.profilePhoto');
+
+emailIcomPlaceholder.src = emailIcon;
+phoneIconPlaceholder.src = phoneIcon;
+// profilePhotoPlaceholder.src = profilePhoto;
